f75d959a1aabd890903f3d90d41d92a673c436ad
PatEnMer <patryk.nowak@gmail.com>
2019-12-04 13:47:01 +0100

    multiple inheritance part 2

--- 01_object_and_prototype/04_multiple_inheritance/js/script.js
+++ 01_object_and_prototype/04_multiple_inheritance/js/script.js
@@ -19,11 +19,35 @@ function Rectangle(sideLengths) {
 	this._name = 'Prostokąt';
 }
 
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
 function Square(sideLength) {
 	Rectangle.call(this, [ sideLength, sideLength ]);
 	this._name = 'Kwadrat';
 }
 
+Square.prototype = Object.create(Rectangle.prototype);
+Shape.prototype.constructor = Square;
+
+function Triangle(sideLength) {
+	Shape.call(this, [ sideLength, sideLength, sideLength ]);
+	this._name = 'Trójkąt';
+}
+
+Triangle.prototype = Object.create(Shape.prototype);
+Triangle.prototype.constructor = Triangle;
+
+Triangle.prototype.getArea = function() {
+	let a = this._sideLength[0];
+	return +(a * a * Math.sqrt(3) / 4).toFixed(2); //"+" konwertuje wynikowego z obiektu stringa na number - trick
+};
+
+Triangle.prototype = Shape.prototype;
+
 // const shape1 = new Shape([ 20, 20, 20, 20 ]);
 // const shape1 = new Rectangle([ 20, 10 ]);
-const shape1 = new Square(10);
+// const shape1 = new Square(10);
+const triangle = new Triangle(10);
+const square = new Square(20);
+const rectangle = new Rectangle([ 20, 10 ]);
