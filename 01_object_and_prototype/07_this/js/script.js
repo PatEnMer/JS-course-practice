b7aded6be007393bad2f6df5705677c00e147f09
PatEnMer <patryk.nowak@gmail.com>
2019-12-06 11:10:38 +0100

    m

---
+++ 01_object_and_prototype/07_this/js/script.js
@@ -0,0 +1,29 @@
+function Shape(sideLengths) {
+	if (!(this instanceof Shape)) {
+		return new Shape(sideLengths);
+	}
+
+	this.sideLengths = sideLengths;
+}
+
+function sayHello() {
+	// console.log(this);
+	return this.firstName + ' ' + this.lastName;
+}
+
+// var firstName = 'Jan';
+// var lastName = 'Jankowski';
+
+var person = {
+	firstName: 'Jan',
+	lastName: 'kowalski',
+	sayHello: sayHello
+};
+
+var person2 = {
+	firstName: 'Kasia',
+	lastName: 'Nowak',
+	sayHello: sayHello
+};
+
+var shape = Shape([ 20, 20, 20, 20 ]);
