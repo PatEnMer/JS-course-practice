b7ce4090558a0b2bea9f97b9423760e747d5b145
PatEnMer <patryk.nowak@gmail.com>
2019-12-13 12:25:40 +0100

    event handing

--- 03_AJAX/01_sending_request/js/script.js
+++ 03_AJAX/01_sending_request/js/script.js
@@ -1,11 +1,16 @@
 const xhr = new XMLHttpRequest();
+
 console.log(xhr.readyState);
 
 xhr.open('GET', 'about-us.html', true);
 
 console.log(xhr.readyState);
 
-xhr.send(null);
+xhr.onreadystatechange = function(e) {};
+
+// xhr.send(null);
+
+// console.log(xhr.response);
 
 // 0 - Unsent
 // 1 - Opened
--- 03_AJAX/02_event_handing/js/script.js
+++ 03_AJAX/02_event_handing/js/script.js
@@ -1,12 +1,36 @@
 const xhr = new XMLHttpRequest();
 
-console.log(xhr.readyState);
+// console.log(xhr.readyState);
 
 xhr.open('GET', 'about-us.html', true);
 
-console.log(xhr.readyState);
+// console.log(xhr.readyState);
+
+xhr.onreadystatechange = function(e) {
+	if (this.readyState === 4 && this.status === 200) {
+		console.log(this.response);
+	}
+};
+
+function logType(e) {
+	console.log(e.type);
+}
+
+xhr.timeout = 1;
+
+xhr.onloadstart = logType;
+xhr.onprogress = logType;
+xhr.onabort = logType;
+xhr.onerror = logType;
+xhr.onload = logType;
+xhr.ontimeout = logType;
+xhr.onloadend = logType;
 
 xhr.send(null);
+
+// xhr.abort();
+
+// console.log(xhr.response);
 
 // 0 - Unsent
 // 1 - Opened
