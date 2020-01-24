a16df34c80dfe2cfce24c5abf9094f52c7e3e647
PatEnMer <patryk.nowak@gmail.com>
2020-01-24 13:12:50 +0100

    project finish

--- 05_regular_expressions/08_practise_project/js/script.js
+++ 05_regular_expressions/08_practise_project/js/script.js
@@ -3,17 +3,33 @@
 		this._field = filed;
 		this._words = restrictedWords.split(/, */);
 		this._regex = new RegExp('(' + this._words.join('|') + ')', 'igm');
-		console.log(this._regex);
 
 		this._assignEvents();
 	}
+
+	TrollKiller.prototype._censorSigns = '!@#$%&'.split('');
 
 	TrollKiller.prototype._assignEvents = function() {
 		this._field.addEventListener('keyup', this._filterMessage.bind(this), false);
 	};
 
 	TrollKiller.prototype._filterMessage = function(e) {
-		console.log(e);
+		this._field.value = this._field.value.replace(
+			this._regex,
+			function(match) {
+				return this._censorWord(match);
+			}.bind(this)
+		);
+	};
+
+	TrollKiller.prototype._censorWord = function(word) {
+		let censored = '';
+		let random = 0;
+		for (i = 0; i < word.length; i++) {
+			random = Math.round(Math.random() * (this._censorSigns.length - 1 - 0) + 0);
+			censored += this._censorSigns[random];
+		}
+		return censored;
 	};
 
 	let tk = new TrollKiller(
