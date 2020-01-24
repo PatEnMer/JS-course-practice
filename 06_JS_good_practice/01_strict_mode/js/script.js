b6641d9fe8dee1714b26725cf019646b61af25f3
PatEnMer <patryk.nowak@gmail.com>
2020-01-24 13:41:44 +0100

    strict mode

---
+++ 06_JS_good_practice/01_strict_mode/js/script.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+	// // x = 10;
+	// // console.log(this);
+	// function Person(firstName, lastName) {
+	// 	this.firstName = firstName;
+	// 	this.lastName = lastName;
+	// }
+	// let person = new Person('Jan', 'Janikowski');
+	// if (5 > 2) {
+	// 	function sayHello() {
+	// 		console.log('siema');
+	// 	}
+	// }
+	// sayHello();
+	// function sayHello(firstName, lastName) {
+	// 	console.log(arguments.callee);
+	// }
+	// sayHello('jan', 'janowski');
+
+	delete window.location;
+})();
