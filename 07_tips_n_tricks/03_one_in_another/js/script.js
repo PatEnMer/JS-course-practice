3f1e8520ce88cc7e6aa66ee8126719740337868a
PatEnMer <patryk.nowak@gmail.com>
2020-01-27 18:17:30 +0100

    contains

---
+++ 07_tips_n_tricks/03_one_in_another/js/script.js
@@ -0,0 +1,7 @@
+const div = document.querySelector('#container');
+const p = document.querySelector('.text');
+const link = document.querySelector('.link');
+
+console.log(div.contains(p));
+
+console.log(div.contains(link));
